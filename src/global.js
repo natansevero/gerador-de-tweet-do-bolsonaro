6f11f925721c1c7bcf78261ea05bd7e2ca0522e4
Lucas Brogni <lucasbrogni16@gmail.com>
2019-03-11 17:39:01 -0300

    Add randomic numbers for RT and Likes

--- src/global.js
+++ src/global.js
@@ -1,21 +1,32 @@
+import moment from 'moment'
+const getNumbers = () => {
+    const number = Math.floor(Math.random() * (Math.random() * 1000000 ) + 1 )
+
+    return number.toLocaleString()
+}
+
+const getDate = () => {
+    return moment().format('HH:mm - DD MMM YYYY')
+}
+
 export default {
-	user: {
-		avatar: 'images/bolso.jpg',
-		fullName: 'Jair M. Bolsonaro',
-		twitter: '@jairbolsonaro'
-	},
-	datetime: '04:26 - 6 de mar de 2019',
-	retweets: 65.102,
-	likes: 157.243,
-	comments: 37,
-	avatars: [
-		'images/avatar-1.jpg',
-		'images/avatar-2.jpg',
-		'images/avatar-3.jpg',
-		'images/avatar-4.jpg',
-		'images/avatar-5.jpg',
-		'images/avatar-6.jpg',
-		'images/avatar-7.jpg',
-		'images/avatar-8.jpg'
-	]
+    user: {
+        avatar: 'images/bolso.jpg',
+        fullName: 'Jair M. Bolsonaro',
+        twitter: '@jairbolsonaro'
+    },
+    datetime: getDate(),
+    retweets: getNumbers(),
+    likes: getNumbers(),
+    comments: Math.floor(Math.random() * 10 + 1 ),
+    avatars: [
+        'images/avatar-1.jpg',
+        'images/avatar-2.jpg',
+        'images/avatar-3.jpg',
+        'images/avatar-4.jpg',
+        'images/avatar-5.jpg',
+        'images/avatar-6.jpg',
+        'images/avatar-7.jpg',
+        'images/avatar-8.jpg'
+    ]
 };
